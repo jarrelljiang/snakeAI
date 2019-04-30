5f2d7ad011752ec86e80e45d57c6b5d40f266392
jiang <364002527@qq.com>
2019-04-30 23:04:21 +0800

    update gulpfile.js & add BGM

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename')
+var gulp = require("gulp");
+var htmlmin = require("gulp-htmlmin");
+var cssUglify = require("gulp-minify-css");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
 
-//压缩css
-gulp.task('minifyjs', function () {
-    return gulp.src('*.js')    //需要操作的文件
-        .pipe(rename({ suffix: '.min' }))   //rename压缩后的文件名
-        .pipe(uglify())   //执行压缩
-        .pipe(gulp.dest('js'));   //输出文件夹
+gulp.task("html", function() {
+    var options = {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true
+    };
+    gulp.src("index.html")
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("css", function() {
+    gulp.src("snake.css")
+        .pipe(cssUglify())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("js", function() {
+    gulp.src(["snow.js", "snake.js", "bubbly-bg.js"])
+        .pipe(uglify({ mangle: { toplevel: true } }))
+        .pipe(concat("all.min.js"))
+        .pipe(gulp.dest("dist"));
 });
