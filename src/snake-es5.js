4da74392636b5521ae22926085924d7250774b4a
jiang <364002527@qq.com>
2019-05-12 14:20:02 +0800

    通关后按回车刷新页面

--- src/snake-es5.js
+++ src/snake-es5.js
@@ -1245,9 +1245,9 @@ function Snake() {
 
 document.onkeydown = function (event) {
   //按下回车键，开始/暂停游戏
-  if (snake.body.length == 200) location.reload()
   if (getComputedStyle(gameStart).display == 'none' && getComputedStyle(gameOver).display == 'none') {
     if (event.keyCode == 13) {
+      if (snake.body.length == 200) location.reload()
       if (isBegin == false) {
         if (snake.direct == null) {
           snake.direct = 'R';
